14bb63cf573b88142329fdff2a0ea1419ee432f2
Prokhor Krylov <649342+proshik@users.noreply.github.com>
2019-08-28 17:03:55 +0300

    init commit

---
+++ timing.go
@@ -0,0 +1,52 @@
+package main
+
+import "math"
+
+// The method returns the pace on the kilometer by distance and full time.
+// The method accept parameters distance and seconds.
+//
+// A distance parameter is a number of meters.
+// A time parameter is a number of seconds on the race.
+func Pace(distance int, time int) int {
+	/*
+		0. 10000/1000 = 10
+		1. 2646/10=264.6
+		2. 264.6/60 = 4.41
+		3. 0.41 * 60 = 24.6 =>
+		4. 4 + 24.6 = 4m 24.6s
+	*/
+	// 0
+	tripDistance := float64(distance) / 1000
+	// 1
+	secondsOnLap := float64(time) / tripDistance
+	// 2
+	minutestValue := secondsOnLap / 60
+	// 3
+	minutes := int(minutestValue)
+	seconds := int(math.Round((minutestValue - float64(int64(minutestValue))) * 60))
+	// 4
+	if minutes > 0 {
+		return (minutes * 60) + seconds
+	} else {
+		return seconds
+	}
+}
+
+// The method returns the time of race by distance and average pace on the kilometer.
+
+// A distance parameter is a number of meters.
+// A pace parameter it is number of seconds on the one kilometer.
+func Time(distance int, pace int) int {
+	/*
+		1. 21097/1000 = 21,097
+		2. 280*21.097 = 5907.16
+		3. if 5907.16 >= 3600 -> 5907.16 /60 = 98.4526 (minutes),
+	*/
+
+	// 1
+	raceDistance := float64(distance) / 1000
+	// 2
+	totalSeconds := float64(pace) * raceDistance
+
+	return int(totalSeconds)
+}
---
+++ timing_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPace(t *testing.T) {
+	type args struct {
+		distance int
+		time     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "5000",
+			args: args{distance: 5000, time: 1245},
+			want: 249,
+		},
+		{
+			name: "10000",
+			args: args{distance: 10000, time: 2650},
+			want: 265,
+		},
+		{
+			name: "21097",
+			args: args{distance: 21097, time: 5928},
+			want: 281,
+		},
+		{
+			name: "42195",
+			args: args{distance: 42195, time: 13460},
+			want: 319,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Pace(tt.args.distance, tt.args.time); got != tt.want {
+				t.Errorf("Pace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTime(t *testing.T) {
+	type args struct {
+		distance int
+		pace     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "5000",
+			args: args{distance: 5000, pace: 249},
+			want: 1245,
+		},
+		{
+			name: "10000",
+			args: args{distance: 10000, pace: 265},
+			want: 2650,
+		},
+		{
+			name: "21097",
+			args: args{distance: 21097, pace: 281},
+			want: 5928,
+		},
+		{
+			name: "42195",
+			args: args{distance: 42195, pace: 319},
+			want: 13460,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Time(tt.args.distance, tt.args.pace); got != tt.want {
+				t.Errorf("Time() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
