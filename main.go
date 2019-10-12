1af08ab9ae3376207fcfea4449278b8712609af8
Prokhor Krylov <649342+proshik@users.noreply.github.com>
2019-10-12 11:03:17 +0300

    add simple HTTP handler and the preparation for the tg bot

--- main.go
+++ main.go
@@ -1,118 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"net/url"
-	"strconv"
-	"time"
 )
 
 func main() {
-	// http://localhost:8080/time?distance=10000&pace=4m25s
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		query := r.URL.Query()
+	serveMux := http.NewServeMux()
 
-		// validate query params
-		errs := url.Values{}
-		distanceValue := query.Get("distance")
-		if distanceValue == "" {
-			errs.Add("distance", "field is required")
-		}
+	serveMux.HandleFunc("/time", timeHandler)
+	serveMux.HandleFunc("/pace", paceHandler)
 
-		paceValue := query.Get("pace")
-		if paceValue == "" {
-			errs.Add("pace", "field is required")
-		}
-
-		distance, err := strconv.Atoi(distanceValue)
-		if err != nil {
-			errs.Add("distance", "incorrect type should be number")
-		}
-
-		paceDuration, err := time.ParseDuration(paceValue)
-		if err != nil {
-			errs.Add("pace", err.Error())
-		}
-
-		// TODO figure out it
-		if &paceDuration == nil {
-			errs.Add("pace", "parsed value is nil")
-		}
-
-		if len(errs) > 0 {
-			w.Header().Set("Content-type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			err := json.NewEncoder(w).Encode(errs)
-			if err != nil {
-				log.Printf("Error on build json error: %v", err)
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-
-			return
-		}
-
-		// invoke method
-		resultTime := Time(distance, int(paceDuration.Seconds()))
-
-		// to Duration value
-		result := time.Duration(resultTime) * time.Second
-
-		_, _ = w.Write([]byte(result.String()))
-	})
-
-	// http://localhost:8080/pace?distance=21097&time=1h38m48s
-	http.HandleFunc("/pace", func(w http.ResponseWriter, r *http.Request) {
-		query := r.URL.Query()
-
-		// validate query params
-		errs := url.Values{}
-		distanceValue := query.Get("distance")
-		if distanceValue == "" {
-			errs.Add("distance", "field is required")
-		}
-
-		timeValue := query.Get("time")
-		if timeValue == "" {
-			errs.Add("time", "field is required")
-		}
-
-		distance, err := strconv.Atoi(distanceValue)
-		if err != nil {
-			errs.Add("distance", "incorrect type should be number")
-		}
-
-		timeDuration, err := time.ParseDuration(timeValue)
-		if err != nil {
-			errs.Add("time", err.Error())
-		}
-
-		// TODO figure out it
-		if &timeDuration == nil {
-			errs.Add("pace", "parsed value is nil")
-		}
-
-		if len(errs) > 0 {
-			w.Header().Set("Content-type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			err := json.NewEncoder(w).Encode(errs)
-			if err != nil {
-				log.Printf("Error on build json error: %v", err)
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-
-			return
-		}
-
-		// invoke method
-		resultPace := Pace(distance, int(timeDuration.Seconds()))
-
-		// to Duration value
-		result := time.Duration(resultPace) * time.Second
-
-		_, _ = w.Write([]byte(result.String()))
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Listening ... ")
+	err := http.ListenAndServe(":8080", serveMux)
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ telegram.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
+
+func NewTelegramBot(token string) {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	webHookConfig := tgbotapi.NewWebhook("https://www.localhost:3009")
+
+	_, err = bot.SetWebhook(webHookConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
+}
